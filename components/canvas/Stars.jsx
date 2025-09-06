1c463df86c6374170b8070e7e20bf1bef6863688
jappanjotkaur <168124142+jappanjotkaur@users.noreply.github.com>
2025-09-06 15:27:18 +0530

    Update Stars.jsx

--- components/canvas/Stars.jsx
+++ components/canvas/Stars.jsx
@@ -1,7 +1,6 @@
 import { useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as THREE from "three";
 
 import CanvasLoader from "../Loader";
 
