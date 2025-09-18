f8f58b7628ec1cf01f2bea9d099c998901e9a646
jappanjotkaur <168124142+jappanjotkaur@users.noreply.github.com>
2025-09-18 16:30:38 +0530

    Update Earth.jsx

--- components/canvas/Earth.jsx
+++ components/canvas/Earth.jsx
@@ -1,19 +1,26 @@
 "use client";
 import React, { Suspense, useRef, useState } from "react";
-import Spline from "@splinetool/react-spline";
+import dynamic from "next/dynamic";
+
+const Spline = dynamic(() => import("@splinetool/react-spline"), {
+  ssr: false, // 🚀 disables SSR for Spline
+});
+
 function Earth({ isMobile }) {
   const splineContainer = useRef(null);
   const [splineApp, setSplineApp] = useState(null);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Spline
         ref={splineContainer}
-        scene="/models/planet/untitled3.spline" // 👈 replace with your .spline file
+        scene="/models/planet/untitled3.spline"
         onLoad={(app) => setSplineApp(app)}
       />
     </Suspense>
   );
 }
+
 function EarthCanvas({ isMobile }) {
   return (
     <div className="cursor-pointer w-full h-full">
@@ -21,4 +28,5 @@ function EarthCanvas({ isMobile }) {
     </div>
   );
 }
+
 export default EarthCanvas;
